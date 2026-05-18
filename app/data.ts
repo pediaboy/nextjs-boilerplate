22bd7265367655d6abe991f6c04a8be9f03fa497
pediaboy <thoriqpedia@gmail.com>
2026-05-19 00:09:19 +0700

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -11,27 +11,27 @@ export const myPortfolio = [
 export const classSignals = [
   {
     code: "LCKM",
-    desc: 'Broker XA konsisten nampung di 112.',
+    desc: 'paling favorit buat besok gara" broker XA konsisten nampung di 112 pas sesi post trading tadi',
     entry: 112, antri: 108, sl: 98, tp: 125,
   },
   {
     code: "DEWA",
-    desc: 'MG sm XC jagain harga di 440.',
+    desc: 'volume transaksinya gila"an hari ini tpi MG sm XC keliatan jagain harga di 440 pas akhir sesi',
     entry: 440, antri: 430, sl: 420, tp: 465,
   },
   {
     code: "GSMF",
-    desc: "Kuat nahan di atas 150.",
+    desc: "pola mantulnya udah mulai keliatan buat lanjutin kenaikan mumpung masih kuat nahan di atas 150",
     entry: 155, antri: 150, sl: 148, tp: 165,
   },
   {
     code: "HUMI",
-    desc: "Mau jemput area 200 lagi.",
+    desc: "momentum mantul dari bawah masih kerasa bgt tujuannya jelas mau jemput area 200 lagi",
     entry: 185, antri: 180, sl: 175, tp: 205,
   },
   {
     code: "BNBR",
-    desc: "Manfaatin pantulan abis dikocok.",
+    desc: "volatilitasnya emang liar tpi cocok buat yg mau main cepet manfaatin pantulan abis dikocok hari ini",
     entry: 161, antri: 156, sl: 152, tp: 172,
   },
 ];
